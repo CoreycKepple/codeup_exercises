ba03b97b3220e27f30ece7778cbed81cb199b014
Corey Kepple <coreyckepple@gmail.com>
2014-02-07 12:08:21 -0600

    Added additional functionality and features to high low game

--- php/highlow.php
+++ php/highlow.php
@@ -1,4 +1,7 @@
 <?php
+//Create Instructions
+echo "Welcome to High - Low!\nThe Object of the Game\nis to guess the random number\nbetween 1 and 100\nHave Fun!\n\n\n\n\n";
+
 // Generate Random Number
 $random_number = mt_rand(1,100);
 $guess = 0;
@@ -8,17 +11,27 @@ do {
 	fwrite(STDOUT, 'Please Guess a Number: ');
 	$input = fgets(STDIN);
 	++$guess;
-
-	if ($random_number > $input){
-	echo 'HIGHER!'."\n";
-	}
-	if ($random_number < $input){
-		echo 'LOWER!'."\n";
-	}
-
-} while ($random_number != $input);
+		if ($input< 1 || $input >100){
+		echo "Error please enter a number between 1 and 100\n";
+		}
+			elseif ($random_number > $input){
+			echo 'HIGHER!'."\n";
+			}
+		
+			elseif ($random_number < $input){
+			echo 'LOWER!'."\n";
+			}
+}while ($random_number != $input);
 
 if ($random_number == $input){
  		echo 'CORRECT!'."\n";
  		echo "Total Guesses: $guess\n";
 	} 
+if ($guess <= 5){
+	echo "Great Job -- It took you 5 or less guesses!\n";
+}
+else{echo "Try again to get your guess under 5\n";
+}
+
+
+	?>
