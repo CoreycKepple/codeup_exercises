86aa48d8149a955fdefda55aa92cfa1019667002
Corey Kepple <coreyckepple@gmail.com>
2014-02-07 15:52:22 -0600

    Cleaned up code and Cleaned up comments

--- php/highlow.php
+++ php/highlow.php
@@ -1,12 +1,13 @@
 <?php
-//Check count of arguments to verify the corrent amount have been input
+//Check argument count to verify the correct amount have been input
 if ($argc !=3){
 	echo "Incorrect number of arguments were input -- please only add two numeric values\n";
 	exit(0);
 }
+
 //Confirm to see if range of values will work for game
 if ($argv[1] > $argv[2]){
-	Echo "Error-- The minum range must be higher than maximum range to play\n";
+	Echo "Error-- The minimum range must be higher than maximum range to play\n";
 	exit(0);
 }
 
@@ -19,13 +20,13 @@ if (!is_numeric($argv[2])){
 }
 
 //User will set range values in CLI
-//now convert user CLI input to variable
+//Now convert user CLI input to variable and set variable to integer
 $min = intval($argv[1]);
 $max = intval($argv[2]);
 
 
 //Create Instructions
-// Take User's name
+//Take User's name
 echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\tWelcome to High - Low!\n\n\tThe Object of the Game\n\n   is to guess the random number between\n\n\t       $min and $max \n\n\t       Have Fun!\n\n";
 echo "\nPlease Enter Your Name: ";
 $name = fgets(STDIN);
@@ -54,7 +55,6 @@ do {
 }while ($random_number != $input);
 
 //Give Information regarding the Gameplay
-
 if ($random_number == $input){
  		echo "\n ******CORRECT!*******\n\n";
  		echo "   Total Guesses: $guess\n\n";
