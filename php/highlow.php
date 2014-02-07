4cb35445d28376289a4ee95f9cfee2fd72a8e155
Corey Kepple <coreyckepple@gmail.com>
2014-02-07 11:23:09 -0600

    3rd commit to high low game

--- php/highlow.php
+++ php/highlow.php
@@ -1,11 +1,13 @@
 <?php
 // Generate Random Number
 $random_number = mt_rand(1,100);
+$guess = 0;
 // Prompt User to guess number
 //Evaluate guess based on random number
 do {
 	fwrite(STDOUT, 'Please Guess a Number: ');
 	$input = fgets(STDIN);
+	++$guess;
 
 	if ($random_number > $input){
 	echo 'HIGHER!'."\n";
@@ -13,8 +15,10 @@ do {
 	if ($random_number < $input){
 		echo 'LOWER!'."\n";
 	}
+
 } while ($random_number != $input);
 
 if ($random_number == $input){
  		echo 'CORRECT!'."\n";
+ 		echo "Total Guesses: $guess\n";
 	} 
