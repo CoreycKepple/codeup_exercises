aff72316341bbb124fecbb8509344362feca8e3f
Corey Kepple <coreyckepple@gmail.com>
2014-02-07 15:12:42 -0600

    Added user input to determine range of High Low values -- created failsafes

--- php/highlow.php
+++ php/highlow.php
@@ -1,13 +1,36 @@
 <?php
+//Check count of arguments to verify the corrent amount have been input
+if ($argc !=3){
+	echo "Incorrect number of arguments were input -- please only add two numeric values\n";
+	exit(0);
+}
+//Confirm user input is a number -- if not set it to a number
+if (!is_numeric($argv[1])){
+	$argv[1]=1;
+}
+if (!is_numeric($argv[2])){
+	$argv[2]=100;
+}
+//User will set range values in CLI
+//now convert user CLI input to variable
+$min = intval($argv[1]);
+$max = intval($argv[2]);
+
+
+//Check to determine user inputs were numeric
+if (!is_numeric($min) || !is_numeric($max)){
+	echo "You have entered values which are not numbers -- please enter two numbers to determine game range\n";
+	exit(0);
+}
 //Create Instructions
 // Take User's name
-echo "Welcome to High - Low!\n\nThe Object of the Game\n\nis to guess the random number\n\nbetween 1 and 100\n\nHave Fun!\n\n";
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\tWelcome to High - Low!\n\n\tThe Object of the Game\n\n   is to guess the random number between\n\n\t       $min and $max \n\n\t       Have Fun!\n\n";
 echo "\nPlease Enter Your Name: ";
 $name = fgets(STDIN);
 echo "\n\n";
 
 // Generate Random Number
-$random_number = mt_rand(1,100);
+$random_number = mt_rand($min,$max);
 $guess = 0;
 
 // Prompt User to guess number
@@ -16,8 +39,8 @@ do {
 	fwrite(STDOUT, 'Please Guess a Number: ');
 	$input = fgets(STDIN);
 	++$guess;
-		if ($input< 1 || $input >100){
-		echo "Error please enter a number between 1 and 100\n";
+		if ($input< $min || $input >$max){
+		echo "Error please enter a number between $min and $max\n";
 		}
 			elseif ($random_number > $input){
 			echo 'HIGHER!'."\n";
@@ -31,13 +54,15 @@ do {
 //Give Information regarding the Gameplay
 
 if ($random_number == $input){
- 		echo 'CORRECT!'."\n";
- 		echo "Total Guesses: $guess\n\n";
-	} 
+ 		echo "\n ******CORRECT!*******\n\n";
+ 		echo "   Total Guesses: $guess\n\n";
+} 
+
 if ($guess <= 5){
 	echo "$name\nGreat Job -- It took you 5 or less guesses!\n";
 }
-else{echo "$name\nTry again to get your guess under 5\n";
+
+else{echo "{$name}\nTry again to get your guesses under 5\n";
 }
 exit(0);
 
