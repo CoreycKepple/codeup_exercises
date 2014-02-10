b120804ae9200c8df87ea4ee680cb7804decb2a8
Corey Kepple <coreyckepple@gmail.com>
2014-02-10 15:32:07 -0600

    Generates a random number for day of the week

--- php/switch.php
+++ php/switch.php
@@ -3,27 +3,32 @@
 // Set the default timezone
 date_default_timezone_set('America/Chicago');
 
+//Create variable to randomize day of the week
+$random = mt_rand(1,7);
+
 // Get Day of Week as number
 // 1 (for Monday) through 7 (for Sunday)
-$day_of_week = date('N');
+$day_of_week = date($random);
+
+//Create switch which verifies the day of the week
 
 switch($day_of_week) {
     case 1:
-        echo "Monday\n";
+        echo "\nMonday = :( \n\n";
         break;
     case 2: 
-        echo "Tuesday\n";
+        echo "\nTuesday = :\ \n\n";
         break;
     case 3:
-    	echo "Wednesday\n";
+    	echo "\nWednesday = :/ \n\n";
     	break;
     case 4:
-    	echo "Thursday\n";
+    	echo "\nThursday = :| \n\n";
     	break;
     case 5:
-    	echo "Friday\n";
+    	echo "\nFriday = :) \n\n";
     	break;
     default:
-    	echo "Weekend!!!\n";
+    	echo "\nWeekend!!!\n\n";
     	break;
 }
