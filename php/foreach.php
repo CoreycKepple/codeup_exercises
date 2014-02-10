ca838f84069b4406760341955e695cb18f942c47
Corey Kepple <coreyckepple@gmail.com>
2014-02-10 11:01:58 -0600

    Created loop which displays array values

--- php/foreach.php
+++ php/foreach.php
@@ -1,20 +1,19 @@
 <?php
-
+//Define array to run through foreach loop
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
+//Create foreach loop 
+//Check to determine if array is present within array
+//Create nested foreach loop to display nested array
+
 foreach($things as $thing){
-	if(is_scalar($thing)){
-		if (is_integer($thing)){
-		echo "{$thing} is an Integer!\n";;
-		}
-		elseif (is_float($thing)) {
-		echo "{$thing} is a Float!\n";
-		}
-		elseif (is_bool($thing)) {
-		echo "{$thing} is a Boolean!\n";
-		}
-		elseif (is_string($thing)) {
-		echo "{$thing} is a String!\n";
-		}
-}
+	if(is_array($thing)){
+		echo "ARRAY:\n";
+		foreach ($thing as $array) {
+		echo "\t{$array}\n";
+		};
+	}
+	else{
+		echo "{$thing}\n";		
+	}
 }
