aed1a672fee7b2e6e2bbf72f3f76fbb772b26943
Corey Kepple <coreyckepple@gmail.com>
2014-02-10 10:49:59 -0600

    Changed foreach.php to check first to see if items are scalar

--- php/foreach.php
+++ php/foreach.php
@@ -3,17 +3,18 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach($things as $thing){
-	if (is_integer($thing)){
+	if(is_scalar($thing)){
+		if (is_integer($thing)){
 		echo "{$thing} is an Integer!\n";;
-	}elseif (is_float($thing)) {
+		}
+		elseif (is_float($thing)) {
 		echo "{$thing} is a Float!\n";
-	}elseif (is_bool($thing)) {
+		}
+		elseif (is_bool($thing)) {
 		echo "{$thing} is a Boolean!\n";
-	}elseif (is_array($thing)) {
-		echo "{$thing} is an Array!\n";
-	}elseif (is_null($thing)) {
-		echo "{$thing} is Null!\n";
-	}elseif (is_string($thing)) {
+		}
+		elseif (is_string($thing)) {
 		echo "{$thing} is a String!\n";
-	}
+		}
+}
 }
