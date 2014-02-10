a7b1acd97a352a1918a7fa42694a847fdbf975a9
Corey Kepple <coreyckepple@gmail.com>
2014-02-09 19:12:30 -0600

    Refactored code in for.php to allow user to change increment count

--- php/for.php
+++ php/for.php
@@ -1,18 +1,22 @@
 <?php
 //Prompt user to create starting number
-fwrite(STDOUT,"Please choose the first number in our range: ");
+fwrite(STDOUT,"Please choose the first number in our range:  ");
 $one = fgets(STDIN);
 
 //Prompt User to create ending number
-fwrite(STDOUT, "Please choose the second number in our range:  ");
+fwrite(STDOUT, "Please choose the second number in our range: ");
 $two = fgets(STDIN);
+
+//Prompt user to choose number to increment count by
+fwrite(STDOUT, "Please choose the number you wish to increment your count by: ");
+$incrementer = fgets(STDIN);
 
 //Trim EOL character from input variable
 $trimmed = trim($one,"\n");
 $text = $trimmed;
 
 //Create loop which counts from $one -> $two
-for ($text<$two;$text <= $two;$text+=1){
+for ($text<$two;$text <= $two;$text+=$incrementer){
 echo "{$text}\n";
 }
 
