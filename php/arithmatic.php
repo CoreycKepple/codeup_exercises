56f0d33f9ff877fa915062550b7e071dd58422c3
Corey Kepple <coreyckepple@gmail.com>
2014-02-11 11:17:40 -0600

    made it so arithmetic only displays values which are integers

--- php/arithmatic.php
+++ php/arithmatic.php
@@ -2,30 +2,40 @@
 //Create addition function
 function add($a,$b){
 	$c=$a+$b;
-	echo $c.PHP_EOL;
+	if (is_int($c)){
+		echo $c. PHP_EOL;
+	}
 }
 //Create subtraction function
 function subtract($a,$b){
 	$c=$a-$b;
-	echo $c.PHP_EOL;
+	if (is_int($c)){
+		echo $c. PHP_EOL;
+	}
 }
 //Create multiplication function
 function multiply($a,$b){
 	$c=$a*$b;
-	echo $c.PHP_EOL;
+	if (is_int($c)){
+		echo $c. PHP_EOL;
+	}
 }
 //Create division function
 function divide($a,$b){
 	$c=$a/$b;
-	echo $c.PHP_EOL;
+	if (is_int($c)){
+		echo $c. PHP_EOL;
+	}
 }
 //Create modulus function
 function modulus($a,$b){
 	$c=$a%$b;
-	echo $c.PHP_EOL;
+	if (is_int($c)){
+		echo $c. PHP_EOL;
+	}
 }
 
-add(1,3);
+add(1,3.7837);
 subtract(50,25);
 multiply(3,20);
 divide(50,4);
