9d2633088f89c47d797e2f85ed50dfb89ed18691
Corey Kepple <coreyckepple@gmail.com>
2014-02-12 13:22:41 -0600

    Todo1 is my changed version of todo1.0 -- Todo.php is now TODO 2.0

--- codeup_todo_list/todo.php
+++ codeup_todo_list/todo.php
@@ -3,47 +3,59 @@
 // Create array to hold list of todo items
 $items = array();
 
-// The loop!
-do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {
+// List array items formatted for CLI
+function list_items($list){
+    // Return string of list items separated by newlines.
+    $a="";
+    foreach ($list as $key => $item) {
         $key++;
-        // Display each item and a newline
-        echo "[{$key}] {$item}\n";
+        $a.="[{$key}]{$item}".PHP_EOL;
+    }
+        return $a;
+}
+
+// Get STDIN, strip whitespace and newlines, 
+// and convert to uppercase if $upper is true
+function get_input($upper = FALSE) 
+{
+    if($upper == TRUE){
+    return strtoupper(trim(fgets(STDIN)));
+    }else{
+        return trim(fgets(STDIN));
     }
 
+}
+
+// The loop!
+do {
+    // Echo the list produced by the function
+    echo list_items($items);
+
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit, (D)isplay : ';
+    echo '(N)ew item, (R)emove item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = trim(fgets(STDIN));
+    $input = get_input(TRUE);
 
     // Check for actionable input
-    if ($input == 'N' || $input == 'n' ) {
+    if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
-    } elseif ($input == 'R' || $input == 'r') {
+        $items[] = get_input();
+    } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
-        $key = trim(fgets(STDIN)-1);
+        $key = get_input();
         // Remove from array
-        unset($items[$key]);
+        unset($items[$key-1]);
         //Resets array values when Item is removed from array
         $items = array_values($items);
-    }   elseif ($input == 'D' || $input == 'd') {
-        //display current items
-            if (empty($items)) {
-                echo "\nNo Current Items to Display\n\n\n";
-            }else{
-                null;
-            }
-        }
+    }
 // Exit when input is (Q)uit
-} while ($input != 'Q' && $input != 'q');
+} while ($input != 'Q');
 
 // Say Goodbye!
 echo "Goodbye!\n";
