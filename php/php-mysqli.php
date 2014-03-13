066833dca0cdafec436ceaaecfd0038b98aeadf9
Corey Kepple <coreyckepple@gmail.com>
2014-03-13 14:18:45 -0500

    added @ sign in front of new to only throw my errors

--- php/php-mysqli.php
+++ php/php-mysqli.php
@@ -1,12 +1,12 @@
 <?php
 
 // Get new instance of MySQLi object
-$mysqli = new mysqli('127.0.0.1', 'corey', 'password', 'codeup_mysqli_test_db');
+$mysqli = @new mysqli('127.0.0.1', 'corey', 'password', 'codeup_mysqli_test_db');
 
 // Check for errors
 if ($mysqli->connect_error) {
     
-    echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
+    echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error. PHP_EOL;
 	
 	}else {
 
